256e198fcfe9e5936bccff146d2c247cb1792105
cyberbot143 <krishnasai952@gmail.com>
2020-10-12 00:35:13 -0400

    updates to my app

--- src/App.js
+++ src/App.js
@@ -1,41 +1,384 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import KanbanDB from 'kanbandb/dist/KanbanDB';
+import React, { useState, useEffect } from 'react';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
-function initialize() {
-  /**
-   * 
-   * Use KanbanDB like so (but you might want to move it) - types are provided:
-   * 
-   */
+import { withStyles, makeStyles } from '@material-ui/core/styles';
 
-  KanbanDB.connect();
+import EasyEdit, { Types } from 'react-easy-edit';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
 
-}
+import {
+  MenuItem,
+  TextField,
+  Button,
+  AppBar,
+  Toolbar,
+  Typography,
+} from '@material-ui/core';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
-function App() {
-  // Initialize DB communications.
-  initialize();
+import InputBase from '@material-ui/core/InputBase';
+import kanbanDB from './data';
+
+let db;
+
+(async () => {
+  db = await kanbanDB.connect();
+})();
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(2),
+    display: 'inline-grid',
+    gridTemplateColumns: '1fr 1fr 1fr 1fr',
+  },
+}));
+
+const BootstrapInput = withStyles((theme) => ({
+  root: {
+    'label + &': {
+      marginTop: theme.spacing(3),
+    },
+  },
+  input: {
+    borderRadius: 4,
+    position: 'relative',
+    backgroundColor: theme.palette.background.paper,
+    border: '1px solid #ced4da',
+    fontSize: 16,
+    padding: '10px 26px 10px 12px',
+    transition: theme.transitions.create(['border-color', 'box-shadow']),
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:focus': {
+      borderRadius: 4,
+      borderColor: '#80bdff',
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
+    },
+  },
+}))(InputBase);
+
+const onDragEnd = async (result, columns, setColumns) => {
+  if (!result.destination) return;
+  const { source, destination } = result;
+
+  if (source.droppableId !== destination.droppableId) {
+    const sourceColumn = columns[source.droppableId];
+    const destColumn = columns[destination.droppableId];
+    const sourceItems = [...sourceColumn];
+    const destItems = [...destColumn];
+    const [removed] = sourceItems.splice(source.index, 1);
+    destItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: sourceItems.map((item) => ({
+        ...item,
+        status: source.droppableId,
+      })),
+      [destination.droppableId]: destItems.map((item) => ({
+        ...item,
+        status: destination.droppableId,
+      })),
+    });
+  } else {
+    const column = columns[source.droppableId];
+    const copiedItems = [...column];
+    const [removed] = copiedItems.splice(source.index, 1);
+    copiedItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: copiedItems,
+    });
+  }
+};
+
+const columnsFromBackend = {
+  TODO: [],
+  DOING: [],
+  DONE: [],
+};
+
+const App = () => {
+  const [columns, setColumns] = useState(columnsFromBackend);
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      if (db) {
+        const cards = await db.getCards();
+        setCards(cards);
+      }
+    })();
+  }, []);
+
+  useEffect(() => {}, [cards]);
+
+  const handleSave = async ({ value, columnId, item }) => {
+    const { id, status } = item;
+    await db.updateCardById(id, { description: value });
+    console.log('handle SAve', value, columnId, item);
+    setColumns((prevColumns) => {
+      let newColumns = { ...prevColumns };
+      let idx = newColumns[status].findIndex((x) => x.id === item.id);
+      newColumns[status][idx] = {
+        ...newColumns[status][idx],
+        description: value,
+      };
+      return { ...newColumns };
+    });
+  };
+
+  const cancel = () => {
+    //do nothing
+  };
+
+  const [statusValue, setStatusValue] = React.useState('TODO');
+  const handleChangeStatus = (event) => {
+    setStatusValue(event.target.value);
+  };
+
+  const [descValue, setDescValue] = useState('');
+  const [titleValue, setTitleValue] = useState('');
+
+  const handleChangeDesc = (e) => {
+    setDescValue(e.target.value);
+  };
+
+  const handleChangeTitle = (e) => {
+    setTitleValue(e.target.value);
+  };
+
+  const handleClickRemove = async ({ columnId, item }) => {
+    const { id, status } = item;
+    await db.deleteCardById(id);
+
+    setColumns((prevColumns) => {
+      let newColumns = { ...prevColumns };
+      const updatedItems = newColumns[status].filter(
+        (obj) => obj.id !== item.id
+      );
+      newColumns[status] = updatedItems;
+      return { ...newColumns };
+    });
+    try {
+      const cards = await db.getCards();
+      setCards(cards);
+    } catch (err) {
+      setCards([]);
+    }
+  };
+
+  const handleSaveToBoard = ({ status, description, title }) => async () => {
+    const cardId = await db.addCard({
+      description,
+      status,
+      title,
+    });
+
+    const newCard = await db.getCardById(cardId);
+
+    setColumns((prevColumns) => {
+      let newColumns = { ...prevColumns };
+      newColumns[status].push(newCard);
+      return newColumns;
+    });
+
+    setCards((prevCards) => {
+      let newCardsUpdated = [...prevCards, newCard];
+      return newCardsUpdated;
+    });
+  };
+  const classes = useStyles();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <AppBar
+        position="static"
+        style={{ color: 'white', backgroundColor: '#378a7b' }}
+      >
+        <Toolbar variant="dense">
+          <Typography variant="h6" color="inherit">
+            {'Kanban App'}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      {cards.length > 0 ? (
+        <div
+          style={{
+            display: 'grid',
+            gridTemplateColumns: '1fr 1fr 1fr',
+            height: '100%',
+            width: '100%',
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <DragDropContext
+            onDragEnd={(result) => onDragEnd(result, columns, setColumns)}
+          >
+            {Object.entries(columns).map(([columnId, column], index) => {
+              return (
+                <div
+                  style={{
+                    justifySelf: 'stretch',
+                  }}
+                  key={columnId}
+                >
+                  <h2>{columnId}</h2>
+                  <div style={{ margin: 8 }}>
+                    <Droppable droppableId={columnId} key={columnId}>
+                      {(provided, snapshot) => {
+                        return (
+                          <div
+                            {...provided.droppableProps}
+                            ref={provided.innerRef}
+                            style={{
+                              background: snapshot.isDraggingOver
+                                ? 'lightblue'
+                                : 'lightgrey',
+                              padding: 4,
+                              width: 250,
+                              minHeight: 500,
+                            }}
+                          >
+                            {column.map((item, index) => {
+                              return (
+                                <Draggable
+                                  key={item.id}
+                                  draggableId={item.id}
+                                  index={index}
+                                >
+                                  {(provided, snapshot) => {
+                                    return (
+                                      <div
+                                        ref={provided.innerRef}
+                                        {...provided.draggableProps}
+                                        {...provided.dragHandleProps}
+                                        style={{
+                                          userSelect: 'none',
+                                          padding: 16,
+                                          margin: '0 0 8px 0',
+                                          minHeight: '50px',
+                                          backgroundColor: snapshot.isDragging
+                                            ? '#263B4A'
+                                            : '#456C86',
+                                          color: 'white',
+                                          ...provided.draggableProps.style,
+                                        }}
+                                      >
+                                        <>
+                                          <EasyEdit
+                                            type={Types.TEXT}
+                                            onSave={(value) =>
+                                              handleSave({
+                                                value,
+                                                columnId,
+                                                item,
+                                              })
+                                            }
+                                            onCancel={cancel}
+                                            saveButtonLabel={
+                                              <FontAwesomeIcon icon={faCheck} />
+                                            }
+                                            cancelButtonLabel={
+                                              <FontAwesomeIcon icon={faTimes} />
+                                            }
+                                            value={item.description}
+                                            // instructions="Star this repo!"
+                                          />
+
+                                          <button
+                                            color={'primary'}
+                                            onClick={(e) =>
+                                              handleClickRemove({
+                                                columnId,
+                                                item,
+                                              })
+                                            }
+                                          >
+                                            X
+                                          </button>
+                                        </>
+                                      </div>
+                                    );
+                                  }}
+                                </Draggable>
+                              );
+                            })}
+                            {provided.placeholder}
+                          </div>
+                        );
+                      }}
+                    </Droppable>
+                  </div>
+                </div>
+              );
+            })}
+          </DragDropContext>
+        </div>
+      ) : (
+        <Typography variant="h5">
+          {
+            'No Cards To display , start adding atleast one card to display board'
+          }
+        </Typography>
+      )}
+
+      <FormControl className={classes.margin}>
+        <TextField
+          id="standard-multiline-flexible"
+          label="Title"
+          value={titleValue}
+          onChange={handleChangeTitle}
+        />
+        <TextField
+          id="standard-multiline-flexible"
+          label="Description"
+          multiline
+          rowsMax={2}
+          value={descValue}
+          onChange={handleChangeDesc}
+        />
+
+        <Select
+          labelId="demo-customized-select-label"
+          id="demo-customized-select"
+          value={statusValue}
+          onChange={handleChangeStatus}
+          input={<BootstrapInput />}
+          defaultValue={'TODO'}
+        >
+          {Object.keys(columnsFromBackend).map((item) => (
+            <MenuItem key={item} value={item}>
+              {item}
+            </MenuItem>
+          ))}
+        </Select>
+
+        <Button
+          style={{ color: 'white', backgroundColor: '#378a7b' }}
+          variant="contained"
+          onClick={handleSaveToBoard({
+            status: statusValue,
+            description: descValue,
+            title: titleValue,
+          })}
+        >
+          {'ADD NEW'}
+        </Button>
+      </FormControl>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/data/index.js
@@ -0,0 +1,260 @@
+import { v4 } from 'uuid';
+
+/*
+A card in the database looks like:
+{
+  id: string;
+  name: string;
+  description: string;
+  status: 'TODO' | 'DOING' | 'DONE';
+  created: Date; // UNIX timestamp
+  lastUpdated: Date; // UNIX timestamp
+}
+*/
+
+/**
+ *
+ * @returns {string} Returns the instance ID if you want to reuse across instatiations.
+ */
+function KanbanDB() {
+  // True is uuid has been loaded via import.
+  let ready = false;
+
+  // Unique ID for this particular instance
+  let dbInstanceId;
+
+  // All localStorage items will contain this prefix
+  let dataItemPrefix;
+
+  const CARD_STATUSES = {
+    TODO: 'TODO',
+    DOING: 'DOING',
+    DONE: 'DONE',
+  };
+
+  function createGUID() {
+    return v4();
+  }
+
+  function verifyDbReady() {
+    if (!ready) throw new Error('Database not ready');
+  }
+
+  /**
+   *
+   * @param {string} strDbKey
+   * @returns {string} Key prefixed by unique database instance.
+   */
+  function addPrefix(strDbKey) {
+    return `${dataItemPrefix}--${strDbKey}`;
+  }
+
+  /**
+   * Verify the data structure of the card is valid for usage in database.
+   * @param {Card} card
+   * @returns {boolean}
+   */
+  function isCardValid(card) {
+    // Card must have a name
+    const isValid =
+      card.name &&
+      typeof card.name === 'string' &&
+      card.name.length > 0 &&
+      // If description is provided, it must be a strength w/ a length of at least one
+      card.description
+        ? typeof card.description === 'string' && card.description.length > 0
+        : true &&
+          // If card status is provided, it must be one of the valid statuses
+          (card.status
+            ? Object.keys(CARD_STATUSES).indexOf(card.status) !== -1
+            : true);
+    return isValid;
+  }
+
+  /**
+   * @returns {Promise<Card>} A single card, if found.
+   */
+  this.getCardById = (strId) => {
+    verifyDbReady();
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const card = localStorage.getItem(addPrefix(strId));
+        if (!card) {
+          reject(new Error(`Card with ID ${strId} not found.`));
+        }
+        resolve(JSON.parse(card));
+      }, 100);
+    });
+  };
+
+  /**
+   * @returns {Promise<boolean>} true if succesful.
+   */
+  this.updateCardById = (strId, cardData) => {
+    verifyDbReady();
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        // make sure card exists.
+        const card = localStorage.getItem(addPrefix(strId));
+        if (!card) {
+          reject(new Error(`Card with ID ${strId} not found.`));
+        }
+
+        const newCard = {
+          ...JSON.parse(card),
+          ...cardData,
+          lastUpdated: Date.now(),
+        };
+
+        if (isCardValid(newCard)) {
+          localStorage.setItem(addPrefix(strId), JSON.stringify(newCard));
+          resolve(true);
+        } else {
+          reject(new Error('New card data invalid.'));
+        }
+      }, 100);
+    });
+  };
+
+  /**
+   * @returns {Promise<boolean>} true if succesful.
+   */
+  this.deleteCardById = (strId) => {
+    verifyDbReady();
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        // make sure card exists.
+        const card = localStorage.getItem(addPrefix(strId));
+
+        if (!card) {
+          reject(new Error(`Card with ID ${strId} not found.`));
+        }
+
+        localStorage.removeItem(addPrefix(strId));
+        resolve(true);
+      }, 100);
+    });
+  };
+
+  /**
+   * @returns {Promise<Card[]>} An array of all cards in the database.
+   */
+  this.getCards = () => {
+    verifyDbReady();
+
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const results = [];
+        const keys = Object.keys(localStorage);
+        if (keys.length < 1) {
+          reject(new Error('No data found.'));
+        }
+        const filtered = keys.filter(
+          (strKey) => strKey.indexOf(dataItemPrefix) > -1
+        );
+
+        filtered.forEach((key) => {
+          // we don't add prefix here because key is already fully qualified
+          const item = localStorage.getItem(key);
+          results.push(JSON.parse(item));
+        });
+
+        resolve(results);
+      }, 100);
+    });
+  };
+
+  /**
+   * @param {string[]} arrStatusCodes An array of valid status codes.
+   * @returns {Promise<Card[]>} An array of all cards with specific status codes.
+   */
+  this.getCardsByStatusCodes = (arrStatusCodes) => {
+    verifyDbReady();
+
+    return new Promise((resolve, reject) => {
+      let i;
+      for (i = 0; i < arrStatusCodes.length; i += 1) {
+        if (Object.keys(CARD_STATUSES).indexOf(arrStatusCodes[i]) === -1) {
+          reject(new Error('Invalid status'));
+        }
+      }
+
+      setTimeout(() => {
+        const results = [];
+
+        this.getCards().then((arrCards) => {
+          arrCards.forEach((card) => {
+            if (arrStatusCodes.indexOf(card.status) !== -1) {
+              results.push(card);
+            }
+          });
+
+          resolve(results);
+        });
+      }, 100);
+    });
+  };
+
+  /**
+   * @returns {Promise<string>} A unique ID for the user to recall card again later.
+   */
+  this.addCard = (cardData) => {
+    verifyDbReady();
+
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        // We set the unique ID.
+        const card = {};
+        card.id = createGUID();
+        card.name = cardData.name;
+        card.description = cardData.description;
+        card.status = cardData.status;
+        card.created = Date.now();
+        card.lastUpdated = Date.now();
+
+        if (!isCardValid(card)) {
+          reject(new Error('Invalid card data.'));
+        }
+
+        const cardKey = addPrefix(card.id);
+        localStorage.setItem(cardKey, JSON.stringify(card));
+        resolve(String(card.id));
+      }, 100);
+    });
+  };
+
+  /**
+   * @param {string} previousInstanceId If you want to persist data across instantation, pass
+   * the instance ID from a previous instantiation. Otherwise, every time you instantiate, you
+   * will have a fresh database.
+   * @returns {Promise} A handle to the KanbanDB instance.
+   */
+  this.connect = (previousInstanceId) =>
+    new Promise((resolve) => {
+      ready = true;
+      dbInstanceId = previousInstanceId || createGUID();
+
+      // wipe away any previous data unless it was requested
+      if (!previousInstanceId) {
+        localStorage.clear();
+      }
+
+      dataItemPrefix = `KanbanDB--${dbInstanceId}`;
+
+      resolve(this);
+    });
+
+  // It just nullifies instance.
+  this.disconnect = () =>
+    new Promise((resolve) => {
+      ready = false;
+      dbInstanceId = null;
+      dataItemPrefix = null;
+      resolve(true);
+    });
+
+  // Instance handle.
+  return this;
+}
+
+export default new KanbanDB();
