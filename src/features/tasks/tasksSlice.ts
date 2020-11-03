68782c3d24d976969efea19ca6ea3f8d4030b5a8
Sharlon M. Balbalosa <sbalbalosa@gmail.com>
2020-11-03 23:38:22 +0800

    note for immer library

--- src/features/tasks/tasksSlice.ts
+++ src/features/tasks/tasksSlice.ts
@@ -111,6 +111,7 @@ export const tasksSlice = createSlice({
   name: "tasks",
   initialState,
   reducers: {
+    // BIG NOTE THIS LIBRARY USES IMMER UNDER THE HOOD
     // Redux Toolkit allows us to write "mutating" logic in reducers. It
     // doesn't actually mutate the state because it uses the Immer library,
     // which detects changes to a "draft state" and produces a brand new
